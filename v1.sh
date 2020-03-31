d633ed1dd3eb3549684aed87623ff0e2f85178b4
Robby Decosemaeker <robby.decosemaeker@telenet.be>
2020-03-31 07:38:27 +0200

    cleanup

--- v1.sh
+++ v1.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-docker run --rm -it -p 80:80 -v $(pwd)/v1:/usr/share/nginx/html:ro nginx:alpine
+docker run --name website --hostname website.local --rm -it -p 80:80 -v $(pwd)/v1/html:/usr/share/nginx/html:ro nginx:alpine
